96b187d153cf8055023212cc04f672975070b740
Riya Singh <96543329+singhriyaaa@users.noreply.github.com>
2023-09-12 19:21:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//for dice 1//
+var randomNumber1 = Math.floor(Math.random() * 6)+1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.pn - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //optionalstep --> dice1 - dice6
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src" , randomImageSource); //image1 will will behave like random dice
+
+/* fro dice 2*/
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //dice1 - dice6
+
+ document.querySelectorAll("img")[1].setAttribute("src" , randomImageSource2);
+ 
+ if(randomNumber1 > randomNumber2){
+     document.querySelector("h1").innerHTML = " 🚩 Player 1 wins";
+ }
+ else if(randomNumber2 > randomNumber1){
+     document.querySelector("h1").innerHTML = " 🚩 Player 2 wins";
+ }
+else{
+    document.querySelector("h1").innerHTML = " Draw";
+}
